d8e3694eadc64b71eeaf3aaaac364a87b619c8a8
Sergio David Perdomo Rivera <sperdomo@pibox.app>
2022-11-11 05:54:27 -0500

    add render if partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def render_if(condition, record)
+	if condition
+	  render record
+    end
+  end
+
+  def render_partial_if(condition, partial_path, msg = '')
+	if condition
+	  render partial_path
+	else
+	  msg
+	end
+  end
 end
