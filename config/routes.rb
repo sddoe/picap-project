08c0cdb453e5136c158ba71671f80fbc6fed9eaa
Sergio David Perdomo Rivera <sperdomo@pibox.app>
2022-11-11 03:09:04 -0500

    includes new routes for project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'booking#index'
+  get '/cancel_booking/:id', to: 'bookings#cancel_booking', as: 'cancel_booking'
+  get '/local_records/', to: 'bookings#local_records', as: 'local_records'
+  resources :bookings
 end
